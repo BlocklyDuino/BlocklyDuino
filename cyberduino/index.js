b60574e99e91c8d8c4bd15dd965c6de89e056330
Cyberlane <dogbertuk2000@gmail.com>
2016-09-02 23:25:53 +0200

    missed a semi colon

--- cyberduino/index.js
+++ cyberduino/index.js
@@ -10,6 +10,6 @@ app.use('/js', express.static('js'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'));
-})
+});
 
 app.listen(8080, () => console.log('Listening to port 8080'));
