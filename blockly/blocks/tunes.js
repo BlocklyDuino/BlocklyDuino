e31c629bbde5fb74a0af68647a21ff567713def5
Cyberlane <dogbertuk2000@gmail.com>
2016-10-03 23:07:22 +0200

    Added tunes to markup
    also moved some language stuff into the language file

--- blockly/blocks/tunes.js
+++ blockly/blocks/tunes.js
@@ -7,10 +7,10 @@ Blockly.Blocks.tunes.HUE = 230;
 
 Blockly.Blocks['tunes_play_tune'] = {
   init: function() {
-    this.setHelpUrl('http://www.example.com/');
+    this.setHelpUrl(Blockly.Msg.TUNES_HELPURL);
     this.setColour(Blockly.Blocks.tunes.HUE);
     this.appendValueInput("TUNE")
-        .appendField("play tune");
+        .appendField(Blockly.Msg.TUNES_PLAY);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setTooltip('');
@@ -20,7 +20,7 @@ Blockly.Blocks['tunes_play_tune'] = {
 ['mario', 'felix', 'heman', 'kirby', 'morning', 'pony', 'simpsons', 'smurfs', 'reindeer', 'ducktales'].forEach(function(tune, index) {
   Blockly.Blocks['tunes_' + tune] = {
     init: function() {
-      this.setHelpUrl('http://www.example.com/');
+      this.setHelpUrl(Blockly.Msg.TUNES_HELPURL);
       this.setColour(Blockly.Blocks.tunes.HUE);
       this.appendDummyInput()
           .appendField(tune);
