a9366be624f271839a695482ccacd5a7c7ec002c
chocoskewers <jadavdheijden@gmail.com>
2023-06-08 12:17:32 +0200

    Trying to add zoom?

--- blockly/scripts/index.js
+++ blockly/scripts/index.js
@@ -142,14 +142,22 @@ function init() {
 
   var toolbox = document.getElementById('toolbox');
   Blockly.inject(document.getElementById('content_blocks'),
-      {grid:
-          {spacing: 25,
-           length: 3,
-           colour: '#ccc',
-           snap: true},
-       media: '../../media/',
-       toolbox: toolbox});
-
+    {grid:
+      {spacing: 25,
+      length: 3,
+      colour: '#ccc',
+      snap: true},
+    media: '../../media/',
+    toolbox: toolbox,
+    zoom:
+      {controls: true,
+      wheel: true,
+      startScale: 1.0,
+      maxScale: 3,
+      minScale: 0.3,
+      scaleSpeed: 1.2,
+      pinch: true}});
+  
   auto_save_and_restore_blocks();
 
   //load from url parameter (single param)
